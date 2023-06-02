dc2e29d9f559e8761b779cb7f72bee79d03c0466
lamsd <huynhlam14091994@gmail.com>
2023-06-02 18:42:56 +0700

    git commit all

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import threading
 from time import sleep
 from shutil import rmtree
 from datetime import datetime
+from zipfile import ZipFile
 
 app = Flask(__name__)
 
@@ -16,13 +17,13 @@ if  os.path.exists("upload"):
 if  os.path.exists("download"):
     rmtree("download")
 
-sleep(5)
+sleep(2)
 
 if not os.path.exists("upload"):
     os.makedirs("upload")
 if not os.path.exists("download"):
     os.makedirs("download")
-sleep(5)
+sleep(1)
 
 UPLOAD_FOLDER = Path('upload')
 OUTPUT_FOLDER = Path('download')
@@ -63,9 +64,32 @@ def process_conversion(file, file_path, time_data):
     response = {'success': True, 'message': 'Conversion in progress', 'download_link': download_link}
     return jsonify(response)
 
+def process_conversionALL(file, file_name, file_path, time_data):
+    file.save(file_path)
+    path_file = os.path.join(UPLOAD_FOLDER, time_data, file_name)
+    with ZipFile(file_path, 'r') as zObject:
+        zObject.extractall(path=path_file)
+    zObject.close()
+    for list_name in os.listdir(path_file):
+        # output_file = os.path.join(path_file, list_name.split(".doc")[0]+".pdf")#.with_suffix('.pdf')
+        input_file = os.path.join(app.config['UPLOAD_FOLDER'], time_data, file_name, list_name)
+        output_file =  list_name.split(".doc")[0]+".pdf"
+        output_filename = os.path.join(app.config['OUTPUT_FOLDER'], time_data, file_name, output_file)#os.path.basename(output_file))
+        with task_lock:
+            task_status[input_file] = 'in_progress'
+
+        # Start a new task for file conversion
+        task_queue.put((input_file, output_filename))
+
+    download_link = "/download?filename={}/{}".format(time_data, file_name)
+
+    response = {'success': True, 'message': 'Conversion in progress', 'download_link': download_link}
+    return jsonify(response)
+
 def conversion_worker():
     while True:
         file_path, output_filename = task_queue.get()
+        print("R111 {}".format(file_path))
 
         if file_path not in task_status or task_status[file_path] != 'in_progress':
             task_queue.task_done()
@@ -91,13 +115,33 @@ def index():
 def convert():
     if 'file' in request.files:
         data = request.files["file"]
-        filename = secure_filename(data.filename)
-        time_data = str(datetime.now())
-        file_path = UPLOAD_FOLDER  / filename
-        return process_conversion(data, file_path, time_data)
+        if data.filename.endswith(".zip"):
+            filename = secure_filename(data.filename)
+            time_data = str(datetime.now())
+            file_path = UPLOAD_FOLDER  / filename
+            return process_conversionALL(data, filename, file_path, time_data)
+        else:
+            filename = secure_filename(data.filename)
+            time_data = str(datetime.now())
+            file_path = UPLOAD_FOLDER  / filename
+            return process_conversion(data, file_path, time_data)
 
     response = {'success': False, 'message': 'Invalid file extension'}
     return jsonify(response)
+
+# @app.route('/convertFolder', methods=['POST'])
+# def convertFolder():
+#     if 'file' in request.files:
+#         data = request.files["file"]
+#         if data.endswith(".zip"):
+            
+#         filename = secure_filename(data.filename)
+#         time_data = str(datetime.now())
+#         file_path = UPLOAD_FOLDER  / filename
+#         return process_conversion(data, file_path, time_data)
+
+#     response = {'success': False, 'message': 'Invalid file extension'}
+#     return jsonify(response)
 
 
 @app.route('/download', methods=['GET'])
