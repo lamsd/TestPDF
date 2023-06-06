aa4db869afa8e93b5edfbf63e5a199779dac8050
lamsd <huynhlam14091994@gmail.com>
2023-06-06 13:46:43 +0700

    upload  flask

--- install1.sh
+++ install1.sh
@@ -12,7 +12,7 @@ echo "Starting"
 sudo -S <<< "$password" apt-get update -y
 sudo -S <<< "$password" apt-get upgrade -y
 
-sleep 5
+
 rm -rf "$OFFICE.zip" "$OFFICE"
 
 myArray=("git" "python3-pip" "vim" "curl" "wget" "unzip")
@@ -24,15 +24,12 @@ for package_name in ${myArray[@]}; do
         echo "Installed the package $package_name."  
     fi
 done
-sleep 5
+
 myArray1=("gdown" "flask")
-for package_name in ${myArray1[@]}; do
-    if ! pip3 show "$package_name" &> /dev/null; then
-        pip3 install "$package_name"
-    fi
-done
-sleep 5
+sudo -S <<< "$password" pip3 install gdown
+pip3 install flask
 gdown 'https://drive.google.com/uc?id='"$IDFILE" -O "$OFFICE.zip"
+sleep 5
 
 unzip  "$OFFICE.zip" > /dev/null 2>&1
 sudo -S <<< "$password" apt autoremove -y 
@@ -41,15 +38,16 @@ package_name=( "libreoffice7.5" "libreoffice")
 for package_name1 in ${package_name[@]}; do
     if dpkg -s "$package_name1" >/dev/null 2>&1; then
         # echo "Failed to install the package $package_name1." 
-        sudo -S <<< "$password" apt-get remove "$package_name1" -y   > /dev/null 2>&1
+        sudo -S <<< "$password" apt-get remove "$package_name1" -y  > /dev/null 2>&1
     fi
 done
 sleep 5
-cd  "$OFFICE"
-sudo -s <<< apt-get install libreoffice -y  > /dev/null 2>&1
-sudo -S <<< "$password" apt autoremove -y > /dev/null 2>&1
+sudo -S <<< "$password" apt-get update -y
+sudo -S <<< apt-get install libreoffice -y  
 sleep 5
-sudo -S <<< "$password" dpkg -i lib*.deb > /dev/null 2>&1
+cd  "$OFFICE"
+sudo -S <<< "$password" apt autoremove -y > /dev/null 2>&1
+sudo -S <<< "$password" dpkg -i libreo*.deb > /dev/null 2>&1
 sudo -S <<< "$password" apt-get install -f
 libreoffice7.5 --version > /dev/null 2>&1
 cd ..
